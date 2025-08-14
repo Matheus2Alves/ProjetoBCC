ef91299550d537fa7b524f097a8eddef9622ad48
Matheus <matheuslaw001@gmail.com>
2025-08-14 12:59:52 -0300

    updateFinal

--- teste.py
+++ teste.py
@@ -1,21 +1,26 @@
 import re
 
-user_password = input("Digite uma senha: \n")
+user_password = input("Digite uma senha para verificação: \n")
+erros = []
 
-def verificarSenha(user_password):
+if len(user_password) < 8:
+    erros.append("A senha deve ter pelo menos 8 caracteres.")
 
-    if len(user_password) < 8:
-        return False
+if not re.search(r'\d', user_password):
+    erros.append("A senha deve conter pelo menos um numero.")
 
-    if not re.search(r'\d', user_password): 
-       return False 
+if not re.search(r'[., ;: ` ~! @#$%^&*()_+=|{}[\]-]', user_password):
+    erros.append("A senha deve conter pelo menos um caractere especial (ex: !, @, #, $).")
 
-    if not re.search(r'[.,;:?!(){}<>^]', user_password):
-        return False
+if not re.search(r'[a-z]', user_password) or not re.search(r'[A-Z]', user_password):
+    erros.append("A senha deve conter letras maiúsculas e minusculas.")
 
-    return True
-
-if verificarSenha(user_password):
-    print(f"'{user_password}' é uma senha forte")
+if not erros:
+    print(f"'{user_password}' e uma senha forte!")
 else:
-    print(f"'{user_password}' é uma senha fraca")
+    print(f"'{user_password}' é uma senha fraca.")
+    print("\nMotivos e Sugestões:")
+    for erro in erros:
+        print(f". {erro}")
+
+        
